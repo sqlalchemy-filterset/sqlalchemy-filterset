8d5cf77d26f32a1c5a5dd49bf45c18030744f91b
Yuri Mezentsev <y.mezentsev@idaproject.com>
2022-10-31 19:22:17 +0300

    feat(range_filter): Added literal_binds=True to tests

--- tests/filters/test_range_filter.py
+++ tests/filters/test_range_filter.py
@@ -15,56 +15,64 @@ class TestRangeFilterBuildSelect(AssertsCompiledSQL):
     @pytest.mark.parametrize(
         "value, expected_filtering",
         [
-            ([1, 10], "item.area >= :area_1 AND item.area <= :area_2"),
-            ((10, 1), "item.area >= :area_1 AND item.area <= :area_2"),
-            ((None, 1), "item.area <= :area_1"),
-            ((1, None), "item.area >= :area_1"),
+            ([1, 10], "item.area >= 1 AND item.area <= 10"),
+            ((10, 1), "item.area >= 10 AND item.area <= 1"),
+            ((None, 1), "item.area <= 1"),
+            ((1, None), "item.area >= 1"),
         ],
     )
     def test_filtering(self, value: Any, expected_filtering: str) -> None:
         filter_ = RangeFilter(Item.area)
         stmt = filter_.filter(select(Item.id), value)
-        self.assert_compile(stmt, f"SELECT item.id FROM item WHERE {expected_filtering}")
+        self.assert_compile(
+            stmt, f"SELECT item.id FROM item WHERE {expected_filtering}", literal_binds=True
+        )
 
     @pytest.mark.parametrize(
         "value, expected_filtering",
         [
-            ([1, 10], "item.area < :area_1 OR item.area > :area_2"),
-            ((None, 1), "item.area > :area_1"),
-            ((1, None), "item.area < :area_1"),
+            ([1, 10], "item.area < 1 OR item.area > 10"),
+            ((None, 1), "item.area > 1"),
+            ((1, None), "item.area < 1"),
         ],
     )
     def test_exclude(self, value: Any, expected_filtering: str) -> None:
         filter_ = RangeFilter(Item.area, exclude=True)
         stmt = filter_.filter(select(Item.id), value)
-        self.assert_compile(stmt, f"SELECT item.id FROM item WHERE {expected_filtering}")
+        self.assert_compile(
+            stmt, f"SELECT item.id FROM item WHERE {expected_filtering}", literal_binds=True
+        )
 
     @pytest.mark.parametrize(
         "value, expected_filtering",
         [
-            ([1, 10], "item.area > :area_1 AND item.area < :area_2"),
-            ((None, 1), "item.area < :area_1"),
-            ((1, None), "item.area > :area_1"),
+            ([1, 10], "item.area > 1 AND item.area < 10"),
+            ((None, 1), "item.area < 1"),
+            ((1, None), "item.area > 1"),
         ],
     )
     def test_filtering_with_operator(self, value: Any, expected_filtering: str) -> None:
         filter_ = RangeFilter(Item.area, left_operator=op.gt, right_operator=op.lt)
         stmt = filter_.filter(select(Item.id), value)
-        self.assert_compile(stmt, f"SELECT item.id FROM item WHERE {expected_filtering}")
+        self.assert_compile(
+            stmt, f"SELECT item.id FROM item WHERE {expected_filtering}", literal_binds=True
+        )
 
     @pytest.mark.parametrize(
         "value, expected_filtering",
         [
             # param > left_val AND param <= right_val --> param <= left_val OR param > right_val
-            ([1, 10], "item.area <= :area_1 OR item.area > :area_2"),
-            ((1, None), "item.area <= :area_1"),  # param > left_val --> param <= left_val
-            ((None, 1), "item.area > :area_1"),  # param <= right_val --> param > right_val
+            ([1, 10], "item.area <= 1 OR item.area > 10"),
+            ((1, None), "item.area <= 1"),  # param > left_val --> param <= left_val
+            ((None, 1), "item.area > 1"),  # param <= right_val --> param > right_val
         ],
     )
     def test_filtering_with_operator_and_exclude(self, value: Any, expected_filtering: str) -> None:
         filter_ = RangeFilter(Item.area, left_operator=op.gt, exclude=True)
         stmt = filter_.filter(select(Item.id), value)
-        self.assert_compile(stmt, f"SELECT item.id FROM item WHERE {expected_filtering}")
+        self.assert_compile(
+            stmt, f"SELECT item.id FROM item WHERE {expected_filtering}", literal_binds=True
+        )
 
     @pytest.mark.parametrize("value", [None, [], [None, None]])
     def test_no_filtering(self, value: Any) -> None:
