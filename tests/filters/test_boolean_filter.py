7f1281b98834e7442113b06c710f5cb28e940eeb
Yuri Mezentsev <yurmezence@gmail.com>
2023-01-12 21:25:38 +0300

    feat: boolean filter tests, in/notin tests more coverage

---
+++ tests/filters/test_boolean_filter.py
@@ -0,0 +1,29 @@
+from typing import Any
+
+import pytest
+from sqlalchemy import select
+from sqlalchemy.orm import QueryableAttribute
+from sqlalchemy.testing import AssertsCompiledSQL
+
+from sqlalchemy_filterset.filters import BooleanFilter
+from tests.models import Item
+
+
+class TestBooleanFilterBuildSelect(AssertsCompiledSQL):
+    __dialect__: str = "default"
+
+    @pytest.mark.parametrize(
+        "field, value, expected",
+        [
+            (Item.is_active, True, "item.is_active IS 1"),
+            (Item.is_active, False, "item.is_active IS 0"),
+            (Item.is_active, None, "item.is_active IS NULL"),
+        ],
+    )
+    def test_filtering(self, field: QueryableAttribute, value: Any, expected: str) -> None:
+        filter_ = BooleanFilter(field)
+        self.assert_compile(
+            filter_.filter(select(Item.id), value, {}),
+            f"SELECT item.id FROM item WHERE {expected}",
+            literal_binds=True,
+        )
---
+++ tests/filters/test_in_filter.py
@@ -0,0 +1,58 @@
+from typing import Any
+
+import pytest
+from sqlalchemy import select
+from sqlalchemy.orm import QueryableAttribute
+from sqlalchemy.testing import AssertsCompiledSQL
+
+from sqlalchemy_filterset.filters import InFilter, NotInFilter
+from tests.models import Item
+from tests.models.base import ItemType
+
+
+class TestInFilterBuildSelect(AssertsCompiledSQL):
+    __dialect__: str = "default"
+
+    @pytest.mark.parametrize(
+        "field, value, expected",
+        [
+            (Item.name, ["foo"], "item.name IN ('foo')"),
+            (Item.name, [""], "item.name IN ('')"),
+            (Item.type, [ItemType.foo], "item.type IN ('foo')"),
+            (Item.name, [ItemType.foo.value], "item.name IN ('foo')"),
+            (Item.name, ["foo", "bar"], "item.name IN ('foo', 'bar')"),
+            (Item.name, [], "item.name IN (NULL) AND (1 != 1)"),
+            (Item.name, (), "item.name IN (NULL) AND (1 != 1)"),
+        ],
+    )
+    def test_filtering(self, field: QueryableAttribute, value: Any, expected: str) -> None:
+        filter_ = InFilter(field)
+        self.assert_compile(
+            filter_.filter(select(Item.id), value, {}),
+            f"SELECT item.id FROM item WHERE {expected}",
+            literal_binds=True,
+        )
+
+
+class TestNotInFilterBuildSelect(AssertsCompiledSQL):
+    __dialect__: str = "default"
+
+    @pytest.mark.parametrize(
+        "field, value, expected",
+        [
+            (Item.name, ["foo"], "(item.name NOT IN ('foo'))"),
+            (Item.name, [""], "(item.name NOT IN (''))"),
+            (Item.type, [ItemType.foo], "(item.type NOT IN ('foo'))"),
+            (Item.name, [ItemType.foo.value], "(item.name NOT IN ('foo'))"),
+            (Item.name, ["foo", "bar"], "(item.name NOT IN ('foo', 'bar'))"),
+            (Item.name, [], "(item.name NOT IN (NULL) OR (1 = 1))"),
+            (Item.name, (), "(item.name NOT IN (NULL) OR (1 = 1))"),
+        ],
+    )
+    def test_filtering(self, field: QueryableAttribute, value: Any, expected: str) -> None:
+        filter_ = NotInFilter(field)
+        self.assert_compile(
+            filter_.filter(select(Item.id), value, {}),
+            f"SELECT item.id FROM item WHERE {expected}",
+            literal_binds=True,
+        )
